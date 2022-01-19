4ecebd441c9cbf6fe85e56f31511e102f57fe0e5
Andrew Dmytriv <admytriv@jigsawcreate.com>
2022-01-19 11:03:37 +0200

    axios and types

--- src/@fake-db/mock.js
+++ src/@fake-db/mock.js
@@ -1,7 +1,7 @@
-import axios from '@axios'
+import { axiosIns } from '@/plugins/axios'
 import MockAdapter from 'axios-mock-adapter'
 
 // This sets the mock adapter on the axios instance
-const mock = new MockAdapter(axios)
+const mock = new MockAdapter(axiosIns)
 
 export default mock
